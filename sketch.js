cab067a179095a14c56bcc4c10be1ad1e798894d
park-yina <111878820+park-yina@users.noreply.github.com>
2022-12-20 01:46:52 +0900

    1번 sketch
    첫번째 스케치에 대한 파일을 업로드합니다.

---
+++ sketch.js
@@ -0,0 +1,49 @@
+'use strict';
+var tileCount = 20;
+var actRandomSeed = 0;
+
+var actStrokeCap;
+
+function setup() {
+  createCanvas(600, 600);
+
+  actStrokeCap = ROUND;
+}
+
+function draw() {
+  clear();
+  strokeCap(actStrokeCap);
+
+  randomSeed(actRandomSeed);
+
+  for (var gridY = 0; gridY < tileCount; gridY+=5) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+
+      var posX = width / tileCount * gridX;
+      var posY = height / tileCount * gridY;
+
+      var toggle = int(random(0, 10));
+
+      if (toggle == 0) {
+        strokeWeight(mouseX / 20);
+        line(posX, posY, posX + width / tileCount, posY + height / tileCount);
+      }
+      if (toggle == 1) {
+        strokeWeight(mouseY / 20);
+        line(posX, posY + width / tileCount, posX + height / tileCount, posY);
+      }
+    }
+  }
+}
+
+function mousePressed() {
+  actRandomSeed = random(100000);
+}
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+
+  if (key == '1') actStrokeCap = ROUND;
+  if (key == '2') actStrokeCap = SQUARE;
+  if (key == '3') actStrokeCap = PROJECT;
+}
