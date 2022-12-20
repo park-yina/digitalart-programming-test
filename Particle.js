ded4962aa9e37907173c2a068b6a55194c0722c7
park-yina <111878820+park-yina@users.noreply.github.com>
2022-12-20 20:11:32 +0900

    Update Particle.js
    중심점을 추가하기 위하여 particle의 코드를 수정

--- Particle.js
+++ Particle.js
@@ -1,45 +1,40 @@
-class Particle{
-  constructor(position){
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-3, 3), random(-10, 10));
-  this.position = position.copy();
-  this.lifespan = 255;
-}
-
-run(){
-  this.update();
-  this.display();
-  this.restore2();
-  if(mouseIsPressed){
-    stroke("black");
-      strokeWeight(0);
-      fill("black");
-      circle(this.position.x,this.position.y,255);
-    background(0);
+class Mover {
+  constructor(mass, x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(1,0);
+    this.acceleration = createVector(0, 0);
+    this.mass = mass;
   }
-}
-  update(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 3;
-}
 
-// Method to display
-  display(){
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(random(0,255),random(0,255),random(0,255),this.lifespan);
-  ellipse(this.position.x, this.position.y, 12, 12);
-}
-isDead(){
-  return this.lifespan < 0;
-}
-  restore2(){
-    if(this.position.y>=height-12||this.position.x>=width-12||this.position.x<12||this.position.y<12){
-      noStroke();
-      strokeWeight(0);
-      fill("black");
-      circle(this.position.x,this.position.y,12);
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  display() {
+    stroke(0);
+    strokeWeight(0);
+    fill(random(0,255),random(0,255),random(0,255));
+    circle(this.position.x, this.position.y, random(0,10));
+  }
+
+  checkEdges() {
+    if (this.position.x > width) {
+      this.position.x = width;
+      this.velocity.x *= -1;
+    } else if (this.position.x < 0) {
+      this.velocity.x *= -1;
+      this.position.x = 0;
+    }
+    if (this.position.y > height) {
+      this.velocity.y *= -1;
+      this.position.y = height;
     }
   }
 }
