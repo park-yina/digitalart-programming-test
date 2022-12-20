aab263588f56b187961406ffb9b9cc16c6b1e0d2
park-yina <111878820+park-yina@users.noreply.github.com>
2022-12-20 22:03:45 +0900

    Update ParticleSystem.js
    코드 정리 진행

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -1,6 +1,5 @@
 let particles=[];
 let particles2=[];
-let particles3=[];
 class ParticleSystem{
   constructor(position){
   this.origin = position.copy();
@@ -29,16 +28,5 @@ run2(){
     }
   }
 }
-  addParticle3(){
-  particles3.push(new Mouse(this.origin));
-}
-  run3(){
-  for (let i = particles3.length-1; i >= 0; i--){
-    let k = particles3[i];
-    k.run2();
-    if (k.isDead()) {
-      particles3.splice(i, 1);
-    }
-  }
-  }
+
 }
