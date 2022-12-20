87052083f01b8fd8069caba7c5d320f233e94252
park-yina <111878820+park-yina@users.noreply.github.com>
2022-12-20 23:38:25 +0900

    6-1수정 mouse.js
    61-에 맞추어 수정

--- Mouse.js
+++ Mouse.js
@@ -1,77 +1,44 @@
 class Mouse{
   constructor(position){
   this.position = createVector(mouseX,mouseY);
-    this.acc=createVector(random(-0.03,0.03),random(-0.02,0.01));
-    this.velocity = createVector(random(-2, 3), random(-4, 5));
-    this.lifespan = 255;
+    this.acc=createVector(random(-0.03,0.03),random(-0.02,0.02));
+    this.velocity = createVector(random(-3, 3), random(-5, 5));
+    this.lifespan = 30;
   }
      run2(){
     this.update();
     this.display();
+       this.update2();
+       this.display();
+       this.update3();
+       this.display();
     this.click();
-    this.restore();
   }
 
   update(){
      this.velocity.add(this.acc);
   this.position.add(this.velocity);
-  this.lifespan -= 0.5;
+  this.lifespan += 5;
   }
   update2(){
-    this.acc=createVector(random(-0.005,0.005),random(-0.002,0.002));
+    this.acc=createVector(random(-0.05,0.05),random(-0.02,0.02));
     this.velocity=createVector(random(-0.01,0.01),random(-0.05,0.05));
-    this.lifespan=255;
+    this.lifespan=30;
      this.velocity.add(this.acc);
   this.position.add(this.velocity);
-  this.lifespan -= 0.5;
+  this.lifespan += 10;
+  }
+  update3(){
+    this.acc=createVector(random(-0.01,0.01),random(-0.02,1));
+    this.velocity=createVector(random(-0.01,0.5),random(-0.05,0.3));
+    this.lifespan=90;
+     this.velocity.add(this.acc);
+  this.position.add(this.velocity);
+  this.lifespan += 30;
   }
   display(){
-  stroke(200, this.lifespan);
+  stroke(random(0,255),random(0,255));
   strokeWeight(2);
   fill(random(0,255),random(0,255),random(0,255),this.lifespan);
-  circle(this.position.x, this.position.y, random(0,3));
-}
-  display2(){
-    stroke(random(200),this.lifespan);
-    strokeWeight(2);
-  fill(random(0,255),random(0,255),random(0,255),this.lifespan);
-    rect(this.position.x,this.position.y,random(0,12));
-  }
-isDead(){
-  return this.lifespan < 0;
-}
-  click(){ if(mouseIsPressed===true&&this.position.x<mouseX+36||this.position.x>mouseX-36||this.position.y<mouseX+36||this.position.y>mouseY-36){
-      noStroke()
-      strokeWeight(10);
-      fill(0,255);
-      circle(this.position.x,this.position.y,random(0,3));
-      
-  }
-           if(mouseIsPressed===true){
-     this.update();
-             this.update2();
-             this.display();
-     this.display2();
-             this.restore();
-     this.restore2();
-     
-   } 
-  }
-  restore(){
-    if(this.position.y>=height-12||this.position.x>=width-12||this.position.x<12||this.position.y<12){
-      noStroke()
-      strokeWeight(0);
-      fill("black");
-      circle(this.position.x,this.position.y,random(0,3));
-    }
-}
-   restore2(){
-    if(this.position.y>=height-12||this.position.x>=width-12||this.position.x<12||this.position.y<12){
-      noStroke()
-      strokeWeight(0);
-      fill("black");
-    rect(this.position.x,this.position.y,random(0,3));
-      background(0);
-    }
-}
+  circle(this.position.x, this.position.y,random(1,10));
 }
